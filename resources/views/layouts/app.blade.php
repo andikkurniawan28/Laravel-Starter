cf8f02261b8995acc787a6f32c64fac20d1215bd
Andik Kurniawan <andikk362@gmail.com>
2023-06-14 06:52:28 +0700

    change font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,15 +14,18 @@
     <!-- Custom fonts for this template-->
 	<link rel="icon" type="image/png" href="{{ "/app_icon/".$global["app_icon"] }}"/>
     <link href="/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <!-- Custom styles for this template-->
     <link href="/admin/css/sb-admin-2.min.css" rel="stylesheet">
     <link href="/admin/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
     <link href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css" rel="stylesheet">
     <link href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
 
 </head>
 
