4a837ce5437e69976e4aae572026e5b29be5140f
Andik Kurniawan <andikk362@gmail.com>
2023-06-15 15:08:49 +0700

    add icon to app_auth.blade

--- resources/views/layouts/app_auth.blade.php
+++ resources/views/layouts/app_auth.blade.php
@@ -12,6 +12,7 @@
     <title>{{ $global["app_name"] }}</title>
 
     <!-- Custom fonts for this template-->
+	<link rel="icon" type="image/png" href="{{ "/app_icon/".$global["app_icon"] }}"/>
     <link href="/admin/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
